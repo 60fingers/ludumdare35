1d2145ec2d16f4ded39e14e5d2a17a682132ef71
Patrick Dresel <patrick.dresel@gmx.de>
2016-04-16 14:30:26 +0200

    added paths.txt and updated auxiliaries

--- src/Auxiliaries.py
+++ src/Auxiliaries.py
@@ -1,4 +1,19 @@
 import pygame, os
 
 def readImagePathList(self):
-	return [img\ground1.png, img\ground2.png, img\player1.png, img\player2.png]
+	paths = open("paths.txt","r")
+	pathlist = {}
+	lines = []
+	#read all lines from the file
+	for line in paths:
+		line.append(line)
+		
+	paths.close()
+	
+	#write lines in the dictionary
+	i = 0
+	while i < len(lines)
+		pathlist.update({lines[i]:lines[i+1]})
+		i = i+2
+	
+	return pathlist
