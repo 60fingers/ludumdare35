0c2e3430a2415cd3ccd9f9570ed79f58c4fbdb10
Till Fischer <till.fischer.de@googlemail.com>
2016-04-16 15:12:32 +0200

    added 3 static environment object types: AIR, GROUND, WALL

--- src/StaticObject.py
+++ src/StaticObject.py
@@ -1,8 +1,36 @@
 import pygame, os
-import WorldObject from WorldObject
+from  WorldObject import WorldObject
 
 class StaticObject (WorldObject):
 
 	def __init__(self, position, collision, imglist, animated, visible=True):
 		super(StaticObject, self).__init__(position, collision, imglist, animated, visible)
 
+
+class AirObject(StaticObject):
+	
+	def __init__(self, position):
+		super(AirObject, self).__init__(position,
+			collision=False,
+			imglist=[],
+			animated=False,
+			visible=False)
+
+class GroundObject(StaticObject):
+	
+	def __init__(self, position):
+		super(WallObject, self).__init__(position,
+			collision=True,
+			imglist=["ground1", "ground2"]
+			animated=False,
+			visible=True)
+
+class WallObject(StaticObject):
+	
+	def __init__(self, position):
+		super(StaticObject, self).__init__(position,
+			collision=True,
+			imglist=["wall1", "wall2"]
+			animated=False,
+			visible=True)
+			
