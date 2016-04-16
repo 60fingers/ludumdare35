23889eccce053ea5405743f1b5e307a4331798a8
Patrick Dresel <patrick.dresel@gmx.de>
2016-04-16 10:16:20 +0200

    Added empty classes

---
+++ src/MainControll.py
@@ -0,0 +1,11 @@
+import pygame, os
+
+class MainControll:
+
+def __init__(self):
+
+def main(self):
+	while true:
+	
+	
+	
---
+++ src/StaticObject.py
@@ -0,0 +1,5 @@
+import pygame, os
+
+class StaticObject (WorldObject):
+
+def __init__(self):
