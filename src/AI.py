4c98dfd8ccd534ebe3ccdabefb311732089509de
Till Fischer <till.fischer.de@googlemail.com>
2016-04-16 10:16:32 +0200

    enemy class frames

---
+++ src/AI.py
@@ -0,0 +1,6 @@
+import pygame, os
+
+class AI:
+	
+	def __init__(self):
+		print("not implemented")
---
+++ src/Enemy.py
@@ -0,0 +1,6 @@
+import pygame, os
+
+class Enemy(MovableObject):
+	
+	def __init__ (self):
+		print("Enemies not implemented!")
