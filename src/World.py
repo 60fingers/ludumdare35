d2a070aaa947c328f771fdda5546db63411ccf0b
Patrick Dresel <patrick.dresel@gmx.de>
2016-04-16 12:30:24 +0200

    added objectsSurrounding calculation

--- src/World.py
+++ src/World.py
@@ -3,7 +3,27 @@ import pygame, os
 class World:
 
 	def __init__(self):
-
-	def objectsSurrounding(self,x,y):
-
+		self movables = ""
+		self statics = ""
+		#TODO: test wich values to take
+		self levelheight = 10
+		self surroundarea = 10
+	
+	def objectsSurrounding(self,position):
+		surroundings = []
+		x = position[0]
+		# add every static object in range
+		i = ((x*levelheight) - surroundarea)
+		while i < ((x*levelheight) + surroundarea):
+			surroundings.append(movables[i])
+			i++
+		
+		# add every movable object in range
+		for m in movables:
+			if m.position[0] >= (x - surroundarea) & m.position[0] <= (x + surroundarea):
+				surroundings.append(m)
+			
+		return surroundings
+		
 	def nextStep():
+		print("not implemented")
