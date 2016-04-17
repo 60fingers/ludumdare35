0ced05152afc8cb7360262301c549d33740cd71e
Till Fischer <till.fischer.de@googlemail.com>
2016-04-17 21:15:43 +0200

    player and playermodes: format and doku

--- src/Player.py
+++ src/Player.py
@@ -1,4 +1,4 @@
-import pygame, os, datetime, random
+import pygame, os, random
 
 from MovableObject import MovableObject
 from PlayerModes import PlayerHuman, PlayerGepard, PlayerSnake, PlayerBird
@@ -7,21 +7,23 @@ import CONFIG
 
 class Player (MovableObject):
 			
-	currentKeys = "" # leerer String, wird durch passenden String ersetzt
-	lastKeys = "" # siehe oben
-	lastKeyTime = "" # siehe oben, TODO welches Zeitformat verwenden wir hier?	
-	
 			
 	def __init__ (self, position):
 		MovableObject.__init__(self,
 			position,
 			collision=True,
-			imglist=["PlayerRunningLeft1", "PlayerRunningLeft2", "PlayerRunnungRight1", "PlayerRunnungRight2", "PlayerStandingLeft", "PlayerStandingRight"],
+			imglist=["PlayerRunningLeft1",
+					"PlayerRunningLeft2",
+					"PlayerRunnungRight1",
+					"PlayerRunnungRight2",
+					"PlayerStandingLeft",
+					"PlayerStandingRight"],
 			animated=False,
 			frameDuration = 10,
 			currentImg = None,
 			visible=True,
 			maxSpeed=CONFIG.PLAYER_SPEED_HUMAN)
+
 		self.lastShift = 0 # cooldown timer
 		self.currentShape = 0 # Shapes 0=human, 1=gepard, 2=snake, 3=bird
 		self.currentPlayermode = PlayerHuman(self)
@@ -33,11 +35,13 @@ class Player (MovableObject):
 		
 		# Shapeshift
 		timeSinceLastShift = (pygame.time.get_ticks() - self.lastShift)/1000
+
 		if(timeSinceLastShift >= CONFIG.PLAYER_SHAPESHIFT_COOLDOWN):
 		
 			if(keys[pygame.K_SPACE]):
 			
 				self.currentShape=random.randint(0,3)
+				
 				if(self.currentShape == 0):
 					self.currentPlayermode = PlayerHuman(self)
 				elif(self.currentShape == 1):
@@ -48,10 +52,15 @@ class Player (MovableObject):
 					self.currentPlayermode = PlayerBird(self)
 						
 				print("Shift into shape " + str(self.currentShape))
+
 				# Cooldown
 				self.lastShift = pygame.time.get_ticks()
-			
+
+			# end if
+		# end if
+
 		self.currentPlayermode.nextStep(keys)
+
 		MovableObject.nextStep(self)
 		
 		
