f97fb1ec2b6702520712aeefa0b3ba28705b4b12
Patrick Dresel <patrick.dresel@gmx.de>
2016-04-17 16:49:53 +0200

    changes in Playermodes

--- src/Player.py
+++ src/Player.py
@@ -1,6 +1,7 @@
 import pygame, os, datetime, random
 
 from MovableObject import MovableObject
+from PlayerModes import PlayerHuman, PlayerGepard, PlayerSnake, PlayerBird
 
 import CONFIG
 
@@ -9,7 +10,6 @@ class Player (MovableObject):
 	currentKeys = "" # leerer String, wird durch passenden String ersetzt
 	lastKeys = "" # siehe oben
 	lastKeyTime = "" # siehe oben, TODO welches Zeitformat verwenden wir hier?	
-	currentShape = 0 # Shapes 0=human, 1=gepart, 2=snake, 3=bird
 	
 			
 	def __init__ (self, position):
@@ -23,33 +23,33 @@ class Player (MovableObject):
 			visible=True,
 			maxSpeed=CONFIG.PLAYER_SPEED_HUMAN)
 		self.lastShift = 0 # cooldown timer
+		self.currentShape = 0 # Shapes 0=human, 1=gepard, 2=snake, 3=bird
+		self.currentPlayermode = PlayerHuman(self)
 	
 	def nextStep(self, keys):
 		
 		# Shapeshift
 		timeSinceLastShift = (pygame.time.get_ticks() - self.lastShift)/1000
-		if(timeSinceLastShift >= 5):
+		if(timeSinceLastShift >= CONFIG.PLAYER_SHAPESHIFT_COOLDOWN):
 		
 			if(keys[pygame.K_SPACE]):
 			
-				currentShape=random.randint(0,3)
-				print("Shift into shape " + str(currentShape))
+				self.currentShape=random.randint(0,3)
+				if(self.currentShape == 0):
+					self.currentPlayermode = PlayerHuman(self)
+				elif(self.currentShape == 1):
+					self.currentPlayermode = PlayerGepard(self)
+				elif(self.currentShape == 2):
+					self.currentPlayermode = PlayerSnake(self)
+				elif(self.currentShape == 3):
+					self.currentPlayermode = PlayerBird(self)
+						
+				print("Shift into shape " + str(self.currentShape))
 				# Cooldown
 				self.lastShift = pygame.time.get_ticks()
-				
+			
+		self.currentPlayermode.nextStep(keys)
 		MovableObject.nextStep(self)
 		
 		
 		
-		
-		#if(keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]):
-			#self.position[0] += self.maxSpeed
-		#if(keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]):
-			#self.position[0] -= self.maxSpeed
-		#if(keys[pygame.K_UP] and not keys[pygame.K_DOWN]):
-			#self.position[1] -= self.maxSpeed
-		#if(keys[pygame.K_DOWN] and not keys[pygame.K_UP]):
-			#self.position[1] += self.maxSpeed
-		
-		
-
--- src/PlayerModes.py
+++ src/PlayerModes.py
@@ -1,23 +1,57 @@
-import pygame, os, datetime
+import pygame, os, datetime, CONFIG
 
 
-class PlayerBird (Player):
+
+class PlayerHuman ():
 	
-	def __init__ (self):
+	def __init__ (self, player):
+		self.player = player
+		player.maxSpeed=CONFIG.PLAYER_SPEED_HUMAN
+		
+	def nextStep(self,keys):
+	
+		if(keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]):
+			self.position[0] += self.maxSpeed
+		if(keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]):
+			self.position[0] -= self.maxSpeed
+		if(keys[pygame.K_UP] and not keys[pygame.K_DOWN]):
+			self.position[1] -= self.maxSpeed
+		if(keys[pygame.K_DOWN] and not keys[pygame.K_UP]):
+			self.position[1] += self.maxSpeed
+	
+	
+	
+	
+	
+class PlayerGepard ():
+	
+	def __init__ (self, player):
+		self.player = player
+		player.maxSpeed=CONFIG.PLAYER_SPEED_GEPARD
+		
+	def nextStep(self,keys):
 		print("not implemented")
 
-class PlayerGepard (Player):
+
+
+class PlayerSnake ():
 	
-	def __init__ (self):
+	def __init__ (self, player):
+		self.player = player
+		player.maxSpeed=CONFIG.PLAYER_SPEED_SNAKE
+		
+	def nextStep(self,keys):
+		print("not implemented")
+		
+		
+				
+class PlayerBird ():
+	
+	def __init__ (self, player):
+		self.player = player
+		player.maxSpeed=CONFIG.PLAYER_SPEED_BIRD
+		
+	def nextStep(self,keys):
 		print("not implemented")
 
-class PlayerHuman (Player):
-	
-	def __init__ (self):
-		print("not implemented")
-
-class PlayerSnake (Player):
-	
-	def __init__ (self):
-		print("not implemented")
 
