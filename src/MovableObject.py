2d6d2ea243549e69983c0c3f30fb32df24aeba95
Till Fischer <till.fischer.de@googlemail.com>
2016-04-18 20:41:01 +0200

    collision from below, still buggy, but it's a start

--- src/MovableObject.py
+++ src/MovableObject.py
@@ -76,7 +76,7 @@ class MovableObject(WorldObject):
 				
 				if (self.cbox.top <= obj.cbox.top):
 
-					# from above or from the side!
+					# the player's coming from above or from above or from the side!
 					
 					if (self.cbox.bottom - self.speed[1] < obj.cbox.top):
 						# landed on object - haha, lucky
@@ -114,11 +114,69 @@ class MovableObject(WorldObject):
 							# prevent from running again
 							if (self.speed[0] < 0):
 								self.speed[0] = 0
+
+					# end if (top or side)
+
+				# end if (top or below)
+
+
+				else:
+					# now checking top
+
+					speedComponentRelation = float(self.speed[0]) / self.speed[1]
+
+					crashvector = [0,0]
+					
+					crashedFromLeft = None
+
+					# check, wether the crash was on the left or the right side
+					if (self.cbox.left < obj.cbox.left):
+
+						#crashed from left
+						crashedFromLeft = True
+						crashvector[0] = self.cbox.right - obj.cbox.left
+
+					else:
+						
+						#crashed from right
+						crashedFromLeft = False
+						crashvector[0] = self.cbox.left - obj.cbox.right
+
 					# end if
-				# end if
-			# end if
+
+
+					crashvector[1] = obj.cbox.bottom - self.cbox.top
+
+					crashComponentRelation = float(crashvector[0]) / crashvector[1]
+
+
+					# check if the player crashed into the ceiling or into the wall
+					if (speedComponentRelation > crashComponentRelation):
+						
+						# horizontal correction
+						if (crashedFromLeft):
+							
+							self.move([obj.cbox.left - self.cbox.right, 0])
+
+						else:
+							
+							self.move([obj.cbox.right - self.cbox.left, 0])
+
+
+						self.speed[0] = 0
+
+					else:
+						
+						# vertical correction
+						
+						self.move([0, obj.cbox.bottom - self.cbox.top])
+						self.speed[1] = 0
+
+
+
+			# end if (is there even collision)
 			
-		# end for
+		# end for (searching through all objects in range)
 			
 		
 		self.move(self.speed)
